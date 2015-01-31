c5e96a33fd3d9f1d061b214139afd9eef97b549e
Lucas Calje <lucas@frontmen.nl>
2015-01-31 20:51:56 +0100

    Added better example code

--- di.js
+++ di.js
@@ -88,14 +88,19 @@ exports.DI = (function(console, DEBUG) {
          App.di.registerType("ajax", App.AJAX, [], { singleton: true }) ;
          App.di.registerType("util", App.Util, ["compress", true, ["wsql", "ls"] ], { singleton: true } ) ;
          **/
-        register: function(contract, classRef, params, options)
-        {
-            if ( params && !Array.isArray(params) ) { // fix input
+        register: function(contract, classRef, params, options) {
+            if ( params && !Array.isArray(params) ) // fix input
+            {
                 options = params ;
                 params = [] ;
             }
 
-            this._contracts[contract] = { classRef: classRef, params: params||[], options: options||{} } ;
+            this._contracts[contract] = {
+                classRef: classRef,
+                params: params || [],
+                options: options || {}
+            } ;
+
             return this ;
         },
 
@@ -115,7 +120,8 @@ exports.DI = (function(console, DEBUG) {
         getInstance: function(contract, params) {
             var instance = null ;
 
-            if ( this._contracts[contract]  ) {                                      // it should exist
+            if ( this._contracts[contract]  )
+            {                                      // it should exist
                 if (this._contracts[contract].options.singleton )
                 {
                     instance = getSingletonInstance.call(this, contract, params) ;
@@ -126,12 +132,14 @@ exports.DI = (function(console, DEBUG) {
             }
             return instance ;
         },
+
         /**
          * List all available contracts with their description to <tt>console.log</tt>
          * @method listContracts
          */
         listContracts: function() {
             var keys = Object.keys(this._contracts) ;
+
             keys.sort().forEach(function(v) {
                 console(v + ', ' + this._contracts[v].options.description) ;
             }.bind(this)) ;
@@ -143,13 +151,17 @@ exports.DI = (function(console, DEBUG) {
     /* Create or reuse a singleton instance */
     function getSingletonInstance(contract, params) {
         var config = this._contracts[contract] ;
-        if ( params ) {
+
+        if ( params )
+        {
             config.params = params ;
         }
 
-        if ( config.instance === undefined || params ) {
-            config.instance = createInstance.call(this, contract, config.params);
+        if ( config.instance === undefined || params )
+        {
+            config.instance = createInstance.call(this, contract, config.params) ;
         }
+
         return config.instance ;
     }
 
@@ -160,31 +172,38 @@ exports.DI = (function(console, DEBUG) {
      *    function constructor(instance, array, instance) { .. }
      * */
     function createInstanceList(contract, params) {
-        var constParams = [] ;
+        var i, item
+            , constParams = []
+            , noa = Math.max(params.length,  this._contracts[contract].params.length) ;
 
-        (params||this._contracts[contract].params||[]).forEach( function(c) {
-            if ( Array.isArray(c)) {
-                constParams.push( createInstanceList.call(this, contract, c) ) ;
+        for(i = 0; i < noa; i++)
+        {
+            item = params[i] === undefined ? this._contracts[contract].params[i] : params[i] ;
+
+            if ( Array.isArray(item))
+            {
+                constParams.push( createInstanceList.call(this, contract, item) ) ;
             }
-            else {
-                constParams.push( createInstanceIfContract.call(this, c) ) ;
+            else
+            {
+                constParams.push( createInstanceIfContract.call(this, item) ) ;
             }
-        }.bind(this)) ;
+        }
+
         return constParams ;
     }
 
     function createInstanceIfContract(contract) { // is a contract
-        var constParam = contract
-                , problemContract ;
-        if ( typeof(contract) === 'string' && this._contracts[contract] ) {     // is 'contract' just a contructor parameter or a contract?
-            if ( depCheck.indexOf(contract) === -1 ) {                    // check for circular dependency
-                //this._depCheck.push(contract) ;                               // add contract to circular dependency check list
-                constParam = this.getInstance(contract) ;                       // create the instance
-                depCheck.pop() ;                                          // done, remove dependency from the list
+        var problemContract, constParam = contract
+
+        if ( typeof(contract) === 'string' && this._contracts[contract] ) {   // is 'contract' just a contructor parameter or a contract?
+            if ( depCheck.indexOf(contract) === -1 ) {                        // check for circular dependency
+                constParam = this.getInstance(contract) ;                     // create the instance
+                depCheck.pop() ;                                              // done, remove dependency from the list
             }
             else { // circular dependency detected!! --> STOP, someone did something stupid -> fix needed!!
                 problemContract = depCheck[0] ;
-                depCheck.length = 0 ;                                     // cleanup
+                depCheck.length = 0 ;                                         // cleanup
                 throw "Circular dependency detected for contract " + problemContract ;
             }
         }
@@ -203,14 +222,12 @@ exports.DI = (function(console, DEBUG) {
      * @example
      var storage = App.di.createInstance("data", ["compress", true, "websql"]) ;
      **/
-    function createInstance(contract, params)
-    {
-        var instance = null
-                , self = this
-                , cr ;
+    function createInstance(contract, params) {
+        var cr, instance = null
+            , self = this ;
 
         function Dependency(){
-            cr.apply(this, createInstanceList.call(self, contract, params)) ;
+            cr.apply(this, createInstanceList.call(self, contract, params||[])) ;
         }
 
         if ( this._contracts[contract]) {           // contract should exist
@@ -231,7 +248,8 @@ exports.DI = (function(console, DEBUG) {
 })(console, typeof DEBUG === 'undefined' ? false : DEBUG) ;
 
 // AMD compatible
-if (typeof window !== 'undefined' && typeof window.define === "function" && window.define.amd) {
+if (typeof window !== 'undefined' && typeof window.define === "function" && window.define.amd)
+{
     window.define('DI', [], function () {
         return window.DI;
     });
