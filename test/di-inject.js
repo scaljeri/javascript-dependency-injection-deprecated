73bae66f239b7eb03982a4c958e95ec087501e86
Lucas Calje <lucas@calje.eu>
2017-01-13 22:40:22 +0100

    ???

--- test/di-inject.js
+++ test/di-inject.js
@@ -20,9 +20,30 @@ describe("DI", () => {
         di.register('$foo', fixtures.Foo);
       });
 
+      it('should have setup the contract', () => {
+        let contract = di.contracts['$barSimple'];
+
+        testContract(contract, {
+          classRef: fixtures.BarSimple,
+          options: {},
+          params: [],
+          paramsOrigin: 'auto'
+        });
+      });
+
       it('should initialize without injection', () => {
         di.getInstance('$barSimple').should.exist;
       });
+
     });
   });
 });
+
+function testContract(contract, match) {
+  contract.should.exist;
+  contract.classRef.should.equals(match.classRef);
+  contract.params.should.eql(match.params);
+  contract.paramsOrigin.should.equals(match.paramsOrigin);
+  contract.options.should.eql(match.options);
+
+}
